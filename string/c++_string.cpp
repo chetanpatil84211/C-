538ffc0eddf542d47d568c82800cc72c6b870896
chetanpatil84211 <erchetanpatil868@gmail.com>
2024-12-12 23:37:51 +0530

    C++

---
+++ string/c++_string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+  {
+     string str1;
+	 str1=("Patil");
+
+	 string str2={"Chetan"};
+
+	 cout<<str1<<endl<<str2<<endl;
+
+	return 0;
+}
---
+++ string/str.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+   string S="C in Depth";
+
+    cout<<S<<endl;
+
+	return 0;
+}
